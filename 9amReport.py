a21845de3c7296030647b7229a7b660d5a3d70e2
verghese96 <verghese96@gmail.com>
2023-10-26 09:43:20 +0800

    First Commit

---
+++ 9amReport.py
@@ -0,0 +1,24 @@
+# importing csv module
+import csv
+ 
+# csv file name
+filename = "aapl.csv"
+ 
+# initializing the titles and rows list
+fields = []
+rows = []
+
+# reading csv file
+with open(filename, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+     
+    # extracting field names through first row
+    fields = next(csvreader)
+ 
+    # extracting each data row one by one
+    for row in csvreader:
+        rows.append(row)
+ 
+    # get total number of rows
+    print("Total no. of rows: %d"%(csvreader.line_num))
