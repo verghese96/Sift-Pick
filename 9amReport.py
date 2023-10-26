e6c31fa4606bb43b34eb6110ec1a88c7fa54830e
verghese96 <verghese96@gmail.com>
2023-10-26 17:57:14 +0800

    Added file selection feature

--- 9amReport.py
+++ 9amReport.py
@@ -1,24 +1,30 @@
-# importing csv module
-import csv
- 
-# csv file name
-filename = "aapl.csv"
- 
-# initializing the titles and rows list
-fields = []
-rows = []
+import csv # import csv module
+import pandas as pd
+import os #File reading module
 
-# reading csv file
-with open(filename, 'r') as csvfile:
-    # creating a csv reader object
-    csvreader = csv.reader(csvfile)
-     
-    # extracting field names through first row
-    fields = next(csvreader)
+#user selection interface modules
+from tkinter import Tk,messagebox 
+from tkinter.filedialog import askopenfilename
+
+#User selects CSV file. Checks if CSV.
+ALLOWED_EXTENSTIONS = {".csv"}
+while True:
+    messagebox.showinfo("Morning!", "Please select a CSV extension file.")
+    filename = askopenfilename()
+    extension = os.path.splitext(filename)[1]
+    if extension in ALLOWED_EXTENSTIONS:
+        break
  
-    # extracting each data row one by one
-    for row in csvreader:
-        rows.append(row)
- 
-    # get total number of rows
-    print("Total no. of rows: %d"%(csvreader.line_num))
+#create the dataframe for manipulation with pandas
+df = pd.read_csv(filename)
+#print(df.columns = df.columns.str.replace(' ', '_')) #replace column name spaces to underscore for the 2 columns
+
+#change spaces in headers to "_"
+
+
+#delete empty value cell in column "Payment Gateway"
+
+#delete "direct" values in column "Merchant Commission Type"
+#print(df.MerchantCommissionType.to_string(index=False))
+
+#Desired output data
